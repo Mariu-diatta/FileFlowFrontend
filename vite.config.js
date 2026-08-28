d4fd736558daeb2dbdcbb52bf5ff8295a5f994fa
Marius_DIATTA <marius@kayfo.games>
2026-08-28 13:56:21 +0200

    resolv bug vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  preview: {
+    allowedHosts: ['fileflowfrontend.onrender.com'],
+  },
+})import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
